108261d3d3392f59d6fd61ea721ba0806cc06f72
Naveen486 <57168204+Naveen486@users.noreply.github.com>
2020-07-15 12:33:48 +0530

    Update hello_go.go

--- hello_go.go
+++ hello_go.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("first branch included")
-	fmt.Println("suggest some changes..dont merge to master branch")
+	fmt.Println("suggest some changes..dont merge to master branch.")
 }
